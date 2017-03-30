4e75d6cf0948395288bc6b510cfc4d99c724d2fe
PudiPudi <ludovic.marchand@student.ehb.be>
2017-03-30 17:17:00 +0200

    Speaker: Model + Migration

---
+++ plugins/integration/frontend/models/Speaker.php
@@ -0,0 +1,37 @@
+<?php namespace INTEGRATION\Frontend\Models;
+
+use Model;
+
+/**
+ * Speaker Model
+ */
+class Speaker extends Model
+{
+    /**
+     * @var string The database table used by the model.
+     */
+    public $table = 'integration_frontend_speakers';
+
+    /**
+     * @var array Guarded fields
+     */
+    protected $guarded = ['*'];
+
+    /**
+     * @var array Fillable fields
+     */
+    protected $fillable = [];
+
+    /**
+     * @var array Relations
+     */
+    public $hasOne = [];
+    public $hasMany = [];
+    public $belongsTo = [];
+    public $belongsToMany = [];
+    public $morphTo = [];
+    public $morphOne = [];
+    public $morphMany = [];
+    public $attachOne = [];
+    public $attachMany = [];
+}
