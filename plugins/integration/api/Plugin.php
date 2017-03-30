10c23107fa80d3b4dba38b2da45edd9968194cd0
PudiPudi <ludovic.marchand@student.ehb.be>
2017-03-30 18:32:19 +0200

    api plugin created w/ dependency

---
+++ plugins/integration/api/Plugin.php
@@ -0,0 +1,102 @@
+<?php namespace Integration\API;
+
+use Backend;
+use System\Classes\PluginBase;
+
+/**
+ * API Plugin Information File
+ */
+class Plugin extends PluginBase
+{
+    /**
+     * @var array
+     */
+    public $require = [
+        'Integration.Frontend',
+    ];
+    /**
+     * Returns information about this plugin.
+     *
+     * @return array
+     */
+    public function pluginDetails()
+    {
+        return [
+            'name'        => 'API extension',
+            'description' => 'API Extension for other modules to create content for the frontend to display',
+            'author'      => 'Integration',
+            'icon'        => 'icon-paper-plane-o'
+        ];
+    }
+
+    /**
+     * Register method, called when the plugin is first registered.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+
+    /**
+     * Boot method, called right before the request route.
+     *
+     * @return array
+     */
+    public function boot()
+    {
+
+    }
+
+    /**
+     * Registers any front-end components implemented in this plugin.
+     *
+     * @return array
+     */
+    public function registerComponents()
+    {
+        return []; // Remove this line to activate
+
+        return [
+            'Integration\API\Components\MyComponent' => 'myComponent',
+        ];
+    }
+
+    /**
+     * Registers any back-end permissions used by this plugin.
+     *
+     * @return array
+     */
+    public function registerPermissions()
+    {
+        return []; // Remove this line to activate
+
+        return [
+            'integration.api.some_permission' => [
+                'tab' => 'API',
+                'label' => 'Some permission'
+            ],
+        ];
+    }
+
+    /**
+     * Registers back-end navigation items for this plugin.
+     *
+     * @return array
+     */
+    public function registerNavigation()
+    {
+        return []; // Remove this line to activate
+
+        return [
+            'api' => [
+                'label'       => 'API',
+                'url'         => Backend::url('integration/api/mycontroller'),
+                'icon'        => 'icon-leaf',
+                'permissions' => ['integration.api.*'],
+                'order'       => 500,
+            ],
+        ];
+    }
+}
