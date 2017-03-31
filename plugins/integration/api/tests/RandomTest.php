1aef8145835977f5e3062f5fa58fae19f90491d3
= <=>
2017-03-31 15:49:15 +0200

    Setup testing for the api plugin

---
+++ plugins/integration/api/tests/RandomTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Integration\API\Tests;
+
+use PluginTestCase;
+
+class PostTest extends PluginTestCase
+{
+    public function testCreateFirstPost()
+    {
+        $this->assertEquals(1, 1);
+    }
+}
